1fbd53d8b65913a8d0254ed4dcba48063273e6ca
chenhaiou <chenhaiou@qusong.com>
2024-12-12 10:35:51 +0800

    添加 tests 需要的依赖

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -33,3 +33,10 @@ class TestFieldErrorMessages:
             serializer = ExampleSerializer(data={})
             serializer.is_valid()
             assert serializer.errors['name'][0] == '姓名:这个字段是必填项。', field
+
+        class ExampleSerializer(Serializer):
+            name = field(help_text='姓名', error_messages={'required': '请填入姓名。'})
+
+        serializer = ExampleSerializer(data={})
+        serializer.is_valid()
+        assert serializer.errors['name'][0] == '请填入姓名。'
