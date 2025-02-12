b18df0c4d5f16f0e2faf9d097b0f7600a9cd1d2a
chenhaiou <chenhaiou@qusong.com>
2025-02-12 18:40:09 +0800

    [filters] 增加基础的过滤模型 BaseFilterSet

--- tests/models/modeltest.py
+++ tests/models/modeltest.py
@@ -1,6 +1,8 @@
 from django.db import models
 
+from dseagull.models import BaseModel
 
-class Person(models.Model):
+
+class Person(BaseModel):
     first_name = models.CharField(max_length=30, default='')
     last_name = models.CharField(max_length=30, default='')
