b5a0b5628bae7b4c3456c014dc44abdd3c72c6e9
chenhaiou <chenhaiou@qusong.com>
2025-03-21 13:51:11 +0800

    [dlogging] 接口 500 异常的报错, 支持发送的钉钉

--- tests/test_checks.py
+++ tests/test_checks.py
@@ -14,16 +14,17 @@ class TestChecks(TestCase):
 
         errors = jwt_check(app_configs=None)
         errors = [error.msg for error in errors]
-        self.assertEqual(3, len(errors))
+        self.assertEqual(4, len(errors))
         self.assertIn('请配置 jwt 的加密秘钥 JWT_KEY', errors[0])
         self.assertIn('请配置 jwt 的过期时间(单位秒) JWT_EXP', errors[1])
-        self.assertIn('请配置 DJANGO_REQUEST_ERROR_WEBHOOK', errors[2])
+        self.assertIn('请为 LOGGING 添加配置 django.request', errors[2])
+        self.assertIn('请配置 DJANGO_REQUEST_ERROR_WEBHOOK', errors[3])
 
-    @override_settings(DJANGO_REQUEST_ERROR_WEBHOOK="test")
+    @override_settings(DJANGO_REQUEST_ERROR_WEBHOOK="test", LOGGING={'version': 1, "handlers": {}, 'loggers': {'django.request': {"handlers": []}}})
     def test_django_request_error_webhook(self):
         config = apps.get_app_config('dseagull')
         config.ready()
         errors = jwt_check(app_configs=None)
         errors = [error.msg for error in errors]
-        self.assertEqual(0, len(errors))
+        self.assertEqual(0, len(errors), errors)
         self.assertIn("django.request", settings.LOGGING['loggers'])
