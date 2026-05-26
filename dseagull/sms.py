2143ca8163731e0c7a4c87b63271d30a623be189
chenhaiou <chenhaiou@qusong.com>
2026-05-26 14:15:15 +0800

    [sms] 支持短信发送功能

--- dseagull/sms.py
+++ dseagull/sms.py
@@ -20,21 +20,33 @@ class SMS:
             }
             password = f"{self.account}{self.password}{transaction_id}"
             payload['password'] = hashlib.md5(f"{password}".encode()).hexdigest()
-            payload['content'] = kwargs['content']
             payload['list'] = []
-            for to in kwargs['to_list']:
+            for sign_name, mobile, content in kwargs['to_list']:
                 payload['list'].append({
-                    "mobile": to,
+                    "mobile": mobile,
                     "uuid": uuid.uuid4().hex,
+                    'content': f"【{sign_name}】{content}",
                 })
         else:
             raise ValueError("无法识别 host")
         return payload
 
-    def send(self, to_list: list[str], content: str):
-        payload = self.create_payload(to_list=to_list, content=content)
+    def send(self, to_list: list[tuple]):
+        """
+        :param to_list: [('签名', '手机号', '短信内容'), ]
+        :return:
+        """
+        payload = self.create_payload(to_list=to_list)
         if 'hbsmservice' in self.host:
-            r = httpx.post(f"{self.host}/sms/v2/send-same", json=payload)
-            return r.status_code, r.json()
+            r = httpx.post(f"{self.host}/sms/v2/send-different", json=payload)
+            results = r.json()
+            results['results'] = [
+                {
+                    "mobile": item['mobile'],  # noqa
+                    "uuid": item['uuid'],  # noqa
+                    "content": item['content'],  # noqa
+                } for item in payload['list']
+            ]
+            return r.status_code, results
         else:
             raise ValueError("无法识别 host")
